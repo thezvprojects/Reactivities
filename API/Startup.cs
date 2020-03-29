518f30018edbb5fa8e9c179cd8ed0c7d5a0f1d9f
Žarko Vasojević <53293897+vzaddict@users.noreply.github.com>
2020-03-29 13:52:53 +0200

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,13 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt=>
+            {
+                opt.AddPolicy("CorsPolicy",policy=>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -42,11 +49,16 @@ namespace API
                 app.UseDeveloperExceptionPage();
             }
 
+            
+
+
             // app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
